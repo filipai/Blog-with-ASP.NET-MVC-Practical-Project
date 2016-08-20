2a8e20bddefc65d9205797b936084cd5d6be1d45
filipai <filipailiev@gmail.com>
2016-08-20 19:14:26 +0300

    Modifying the Data Model

--- Blog/Blog/Models/ApplicationUser.cs
+++ Blog/Blog/Models/ApplicationUser.cs
@@ -2,11 +2,16 @@
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blog.Models
 {
     public class ApplicationUser : IdentityUser
     {
+
+        [StringLength(100)]
+        public string FullName { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
